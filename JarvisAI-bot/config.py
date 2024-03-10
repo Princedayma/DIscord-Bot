8b1aabb19eaba8d84ad93aed87f6ee74d16b6a4d
Princedayma <115869150+Princedayma@users.noreply.github.com>
2024-03-10 23:29:54 +0530

    Add files via upload

---
+++ JarvisAI-bot/config.py
@@ -0,0 +1,2 @@
+# todo: Add your api key here
+apikey = "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"
---
+++ JarvisAI-bot/main.py
@@ -0,0 +1,108 @@
+import speech_recognition as sr
+import os
+import webbrowser
+import openai
+from config import apikey
+import datetime
+import random
+import numpy as np
+
+
+
+chatStr = ""
+# https://youtu.be/Z3ZAJoi4x6Q
+def chat(query):
+    global chatStr
+    print(chatStr)
+    openai.api_key = apikey
+    chatStr += f"Harry: {query}\n Jarvis: "
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt= chatStr,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+   # todo: Wrap this inside of a  try catch block
+    say(response["choices"][0]["text"])
+    chatStr += f"{response['choices'][0]['text']}\n"
+    return response["choices"][0]["text"]
+
+# def ai(promt): 
+    openai.api_key = apikey
+    text = f"OpenAI response for Prompt: {prompt}"
+
+    response = openai.ChatCompletion.create(
+  model="text-davinci-003",
+
+  prompt = prompt,
+  temperature=1,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+#print(response["choices"][0]["text"])
+#text +=  response["choices"][0]["text"]
+#if not os.path.exit("openai"):
+#os.mkdir("openai")  work on it 
+
+def say(text):
+    os.system(f'say "{text}"') 
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        # r.pause_threshold =  0.6
+        audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Some Error Occurred. Sorry from Jarvis"
+
+if __name__ == '__main__':
+    print('Welcome to Jarvis A.I')
+    say("Jarvis A.I")
+    while True:
+        print("Listening...")
+        query = takeCommand()
+        # todo: Add more sites
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"],["instagram", "https://www.instagram.com/"]]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir...")
+                webbrowser.open(site[1])
+        # todo: Add a feature to play a specific song
+        if "open music" in query:
+            musicPath = "/Users/harry/Downloads/downfall-21371.mp3"
+            os.system(f"open {musicPath}")
+
+        elif "the time" in query:
+            musicPath = "/Users/harry/Downloads/downfall-21371.mp3"
+            hour = datetime.datetime.now().strftime("%H")
+            min = datetime.datetime.now().strftime("%M")
+            say(f"Sir time is {hour} bajke {min} minutes")
+
+        elif "open facetime".lower() in query.lower():
+            os.system(f"open /System/Applications/FaceTime.app")
+
+        elif "open pass".lower() in query.lower():
+            os.system(f"open /Applications/Passky.app")
+
+        # elif "Using artificial intelligence".lower() in query.lower():
+        #     ai(prompt=query)
+
+        elif "Jarvis Quit".lower() in query.lower():
+            exit()
+
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+
+        else:
+            print("Chatting...")
+            chat(query)
